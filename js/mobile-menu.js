2fef6a3ba68e7de6fb38b925469f7fa7a3558c8a
ValeriiGenn <pvg_erku@ukr.net>
2021-12-15 22:15:14 +0200

    2215

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,4 @@
-/*(() => {
+(() => {
     const mobileMenu = document.querySelector(".js-menu-container");
     const openMenuBtn = document.querySelector(".js-open-menu");
     const closeMenuBtn = document.querySelector(".js-close-menu");
@@ -8,6 +8,7 @@
             openMenuBtn.getAttribute("aria-expanded") === "true" || false;
         openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
         mobileMenu.classList.toggle("is-open");
+
         const scrollLockMethod = !isMenuOpen ?
             "disableBodyScroll" :
             "enableBodyScroll";
@@ -25,17 +26,4 @@
         openMenuBtn.setAttribute("aria-expanded", false);
         bodyScrollLock.enableBodyScroll(document.body);
     });
-})();*/
-(() => {
-    const refs = {
-        openMenuBtn: document.querySelector(".menu-open"),
-        closeMenuBtn: document.querySelector(".menu-close"),
-        menu: document.querySelector(".mobail-menu"),
-    };
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-
-    function toggleMenu() {
-        refs.menu.classList.toggle("is-hidden");
-    }
 })();
